9279c95128ffe781df9cc0767df3d681d4b282c4
jaybenaim <benaimjacob@gmail.com>
2019-09-29 17:32:39 -0400

    added models for store aisle category and product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_193332) do
+ActiveRecord::Schema.define(version: 2019_09_29_213022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2019_09_29_193332) do
     t.index ["aisle_id"], name: "index_categories_on_aisle_id", unique: true
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.text "description"
+    t.text "ingrediants"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -40,4 +51,5 @@ ActiveRecord::Schema.define(version: 2019_09_29_193332) do
 
   add_foreign_key "aisles", "stores"
   add_foreign_key "categories", "aisles"
+  add_foreign_key "products", "categories"
 end
