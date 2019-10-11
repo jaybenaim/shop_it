7b85a4cf5bbc70181b8145d7e497b2bd5b3cc779
jaybenaim <benaimjacob@gmail.com>
2019-10-11 12:24:52 -0400

    added home and navbar components for react

---
+++ app/javascript/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./NavBar";
+
+class Home extends React.Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <div className="container">
+          <Navbar />
+          <h1>Welcome To Shop It</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Home;
---
+++ app/javascript/components/Register.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Register extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Register
